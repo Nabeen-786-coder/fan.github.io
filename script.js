0657cf4283926fc199b0452569cc5df51605d264
Nabeen-786-coder <n26402176@gmail.com>
2024-07-30 11:16:01 -0700

    website

---
+++ script.js
@@ -0,0 +1,18 @@
+let a = document.getElementById("fan")
+
+function myfuncon(){
+    a.style.animationDuration = 3+ "s"
+}
+function myfuncof(){
+    a.style.animationDuration = 0+ "s"
+}
+function myfunc1(){
+    a.style.animationDuration = 1+ "s"
+}
+function myfunc2(){
+    a.style.animationDuration = 0.5+ "s"
+}
+function myfunc3(){
+    a.style.animationDuration = 0.1+ "s"
+}
+
